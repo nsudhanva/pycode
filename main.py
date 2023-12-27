d8355d4118a59d0c599690d50022dee00f161f12
Sudhanva Narayana <nsudhanva@gmail.com>
2023-12-27 12:35:36 -0500

    first chain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 import os
 from dotenv import load_dotenv
 
@@ -9,5 +11,13 @@ api_key = os.environ.get("OPENAI_API_KEY")
 
 llm = OpenAI(openai_api_key=api_key)
 
-result = llm("Write a short poem")
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"],
+)
+
+code_chain = LLMChain(llm=llm, prompt=code_prompt)
+
+result = code_chain({"language": "python", "task": "return a list of numbers"})
+
 print(result)
