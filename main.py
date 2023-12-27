b657aad203e0e7d202311cb4e457036135dd4bb5
Sudhanva Narayana <nsudhanva@gmail.com>
2023-12-27 13:13:08 -0500

    oops

--- main.py
+++ main.py
@@ -1,43 +1,61 @@
+import argparse
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--task", default="return a list of numbers")
-parser.add_argument("--language", default="python")
-args = parser.parse_args()
+class CodeGenerator:
+    def __init__(self, language, task):
+        self.language = language
+        self.task = task
+        self.llm = OpenAI()
+        self.code_chain = self._create_code_chain()
+        self.test_chain = self._create_test_chain()
+
+    def _create_code_chain(self):
+        code_prompt = PromptTemplate(
+            template="Write a very short {} function that will {}".format(
+                self.language, self.task
+            ),
+            input_variables=["language", "task"],
+        )
+        return LLMChain(llm=self.llm, prompt=code_prompt, output_key="code")
+
+    def _create_test_chain(self):
+        test_prompt = PromptTemplate(
+            template="Write a test code for the following {} code:\n{{code}}".format(
+                self.language
+            ),
+            input_variables=["language", "code"],
+        )
+        return LLMChain(llm=self.llm, prompt=test_prompt, output_key="test")
+
+    def generate_code_and_test(self):
+        chain = SequentialChain(
+            chains=[self.code_chain, self.test_chain],
+            input_variables=["language", "task"],
+            output_variables=["code", "test"],
+        )
+        return chain({"language": self.language, "task": self.task})
 
 
-load_dotenv()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task", default="return a list of numbers")
+    parser.add_argument("--language", default="python")
+    args = parser.parse_args()
 
-llm = OpenAI()
+    load_dotenv()
 
-code_prompt = PromptTemplate(
-    template="Write a very short {language} function that will {task}",
-    input_variables=["language", "task"],
-)
+    code_generator = CodeGenerator(language=args.language, task=args.task)
+    result = code_generator.generate_code_and_test()
 
-test_prompt = PromptTemplate(
-    template="Write a test code for the following {language} code:\n{code}",
-    input_variables=["language", "code"],
-)
+    print("Generated Code:")
+    print(result["code"])
+    print("Generated Test:")
+    print(result["test"])
 
-code_chain = LLMChain(llm=llm, prompt=code_prompt, output_key="code")
 
-test_chain = LLMChain(llm=llm, prompt=test_prompt, output_key="test")
-
-chain = SequentialChain(
-    chains=[code_chain, test_chain],
-    input_variables=["language", "task"],
-    output_variables=["code", "test"],
-)
-
-result = chain({"language": args.language, "task": args.task})
-
-print("Generated Code:")
-print(result["code"])
-print("Generated Test:")
-print(result["test"])
+if __name__ == "__main__":
+    main()
