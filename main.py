32a9b7603808e3f907c74d3e303f0b28bf3119a9
Sudhanva Narayana <nsudhanva@gmail.com>
2023-12-27 12:41:39 -0500

    added arg parse

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-import os
-from dotenv import load_dotenv
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", default="return a list of numbers")
+parser.add_argument("--language", default="python")
+args = parser.parse_args()
+
 
 load_dotenv()
 
-
-api_key = os.environ.get("OPENAI_API_KEY")
-
-llm = OpenAI(openai_api_key=api_key)
+llm = OpenAI()
 
 code_prompt = PromptTemplate(
     template="Write a very short {language} function that will {task}",
@@ -18,6 +21,6 @@ code_prompt = PromptTemplate(
 
 code_chain = LLMChain(llm=llm, prompt=code_prompt)
 
-result = code_chain({"language": "python", "task": "return a list of numbers"})
+result = code_chain({"language": args.language, "task": args.task})
 
-print(result)
+print(result["text"])
